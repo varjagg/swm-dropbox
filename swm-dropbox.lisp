7330792635ec8115d11643514b47f088f3e680d0
Eugene Zaikonnikov <eugene@funcall.org>
2015-11-12 22:30:13 +0100

    d oh

--- swm-dropbox.lisp
+++ swm-dropbox.lisp
@@ -6,14 +6,15 @@
 (defvar *dropbox-status* "Checking..")
 
 (defun fetch-status ()
-  (string-right-trim '(#\Space)
-		     (first (split-sequence:split-sequence #\Newline (format nil "~A" (run-shell-command "dropbox status" t))))))
+  (setf *dropbox-status*
+	(string-right-trim '(#\Space)
+		     (first (split-sequence:split-sequence #\Newline (format nil "~A" (run-shell-command "dropbox status" t)))))))
 
 (defun fmt-dropbox (ml)
   "Return dropbox status"
   (declare (ignore ml))
-  *dropbox-status*) 
+  *dropbox-status*)
 
 (add-screen-mode-line-formatter #\x #'fmt-dropbox)
 
-(run-with-timer 0 10 #'fetch-status)
+(run-with-timer 0 10 #'fetch-status) 
